c9867e4804db328d41a0af6cd3553761ecdc6989
indgolymadewebdevelopment <indigolymadewebdevelopment@gmail.com>
2024-10-23 23:21:28 +0100

    masterclass

--- sign-up.js
+++ sign-up.js
@@ -8,7 +8,7 @@ const form = document.forms['submit-to-google-sheet1']
         form.addEventListener('submit', e => {
         e.preventDefault()
         fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => alert("Submission successfull! You will receive a confirmation email from indigolymadewebdevelopment@gmail.com"))
+        .then(response => alert("Submission successfull! You will receive a confirmation email from indigolymadewebdevelopment@gmail.com."))
         .then(response=> form.reset())
         .catch(error => alert('Oops! Check your connection and try resending.'))
 })
