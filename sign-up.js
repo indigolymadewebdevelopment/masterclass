66f3b1327b8682d7f0d7d61a79fc80b7a62ec5f1
indgolymadewebdevelopment <indigolymadewebdevelopment@gmail.com>
2024-10-23 01:16:24 +0100

    masterclass

--- sign-up.js
+++ sign-up.js
@@ -2,12 +2,14 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbx7GU88Tp3r6GRDD74Gjc-8Hpao5c7xJn5e6SJ2e3zchjAnB7814OzJa90IQ6-_rL_P/exec'			
 const form = document.forms['submit-to-google-sheet1']			
 			
-form.addEventListener('submit', e => {			
-e.preventDefault()			
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})			
-.then( alert('Submission successfull!'))
-.then(form.reset())	
-.catch(error => console.error('Error!', error.message))			
-})			
-			
-			
+
+	
+        
+        form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => alert("Submission successfull! You will receive a confirmation email from indigolymadewebdevelopment@gmail.com"))
+        .then(response=> form.reset())
+        .catch(error => alert('Oops! Check your connection and try resending.'))
+})
+        
